f4bae6b5bb86166c18d1b778ff7b6e224d152017
blaze <prabhatkumarverma42@gmail.com>
2024-06-19 06:57:48 +0530

    added treesitter: treesitter-config

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = { "c", "cpp", "python", "java", "lua", "vim", "vimdoc", "javascript", "html" },
+      sync_install = false,
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      autotag = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+  end,
+}
