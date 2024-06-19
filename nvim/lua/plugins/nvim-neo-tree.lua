7d7dff2ec05d7e8241d97afb1a47988615603861
blaze <prabhatkumarverma42@gmail.com>
2024-06-19 06:59:07 +0530

    neo-tree config: side pane file explorer

---
+++ nvim/lua/plugins/nvim-neo-tree.lua
@@ -0,0 +1,15 @@
+return {
+	  "nvim-neo-tree/neo-tree.nvim",
+	  branch = "v3.x",
+    	  dependencies = {
+	  	"nvim-lua/plenary.nvim",
+	  	"nvim-tree/nvim-web-devicons", 
+		"MunifTanjim/nui.nvim",
+    	  },
+	 config = function()
+		vim.keymap.set('n', '<leader>ne', ':Neotree <CR>')
+		vim.keymap.set('n', '<leader>nc', ':Neotree toggle <CR>')
+		vim.keymap.set('n', '<leader>nb', ':Neotree buffers float toggle<CR>')
+		vim.keymap.set('n', '<leader>ng', ':Neotree git_status right toggle<CR>')
+	 end
+}
