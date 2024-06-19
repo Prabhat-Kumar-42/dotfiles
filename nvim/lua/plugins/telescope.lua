f5198ce25172e7bf9305c59987d69043c73822fd
blaze <prabhatkumarverma42@gmail.com>
2024-06-19 06:59:39 +0530

    added telescope: telescope config

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      -- "nvim-tree/native-web-devicons",
+      'folke/todo-comments.nvim',
+    },
+    config = function()
+      local builtin = require("telescope.builtin")
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+
+      telescope.setup({
+        defaults = {
+          path_display = { "smart" },
+          mappings = {
+            i = {
+              ["<C-k>"] = actions.move_selection_previous, -- 'move to previous results'
+              ["<C-j>"] = actions.move_selection_next, -- 'move to next results'
+            },
+          },
+        },
+      })
+
+      telescope.load_extension("fzf")
+
+      vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {desc = 'Find Recent Files'})
+      vim.keymap.set("n", "<leader>fs", builtin.grep_string, {desc = 'Find the string at cursor'})
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {desc = 'Find Files'})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {desc = 'Live Grep'})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {desc = 'Find Buffers'})
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {desc = 'Help Tags'})
+      vim.keymap.set("n", "<leader>ft", '<cmd>TodoTelescope<CR>', {desc = 'Find ToDo'})
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      -- This is your opts table
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
+}
